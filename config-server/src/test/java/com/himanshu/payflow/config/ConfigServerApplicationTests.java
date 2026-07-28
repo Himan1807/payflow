5cf432320a0d52c36a69c09d0845a41ebf2ea008
Himanshu Singh <himanshu.singh2004@gmail.com>
2026-07-28 13:54:51 +0530

    Initialize Config Server, Naming Server and API Gateway

---
+++ config-server/src/test/java/com/himanshu/payflow/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.payflow.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
