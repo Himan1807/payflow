70b18f613f45686caadf1cbc6a067ed1ce52bfe5
Himanshu Singh <himanshu.singh2004@gmail.com>
2026-07-29 17:42:46 +0530

    Create Auth Service

---
+++ auth-service/src/test/java/com/himanshu/payflow/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.payflow.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
